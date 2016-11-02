3469e16d73cc3eee9c14f15b82eb59faeaa4e1b9
Chris Afonso <cafonso@umass.edu>
2016-11-01 22:06:16 -0400

    Modified the view controller for core data

--- KR/CoreDataViewController.swift
+++ KR/CoreDataViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DependentComponentPickerViewController: UIViewController {
+class CoreDataViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
